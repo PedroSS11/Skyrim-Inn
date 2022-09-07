b6b20c619bd24b2fb32a7937758a3e44799fef45
Pedro Henrique <contatophss11@gmail.com>
2022-09-07 16:03:07 -0300

    cidades

--- js/wiki.js
+++ js/wiki.js
@@ -1,13 +1,37 @@
 const linkCity = document.getElementsByClassName('linkLocations')
-const cidades = [{id:1, cidade:'dawnstar', url:'index.html'}]
+//const cidades = [{id:1, cidade:'dawnstar', url:'index.html'}]
+
+
+const dawnstar = document.getElementById('dawnstar')
+const a = () => {
+    let cidades = []
+    for(i in linkCity) {
+    
+    
+        if(linkCity[i].id) {
+    
+            //console.log(linkCity[i].id)
+    
+            cidades.push(linkCity[i].id)
+    
+            console.log(cidades)
+    
+    
+        } 
+    }
+}
+
+a()
+
+
 
 // TIPO ISSOOOOO
-const dawnstar = document.getElementById('dawnstar')
-const abreDawnstar = () => {
-    let site = cidades[0].url
-    window.open(site)
-}
-dawnstar.addEventListener("click", abreDawnstar)
+// const dawnstar = document.getElementById('dawnstar')
+// const abreJanela = () => {
+//     let site = cidades[0].url
+//     window.open(site)
+// }
+// dawnstar.addEventListener("click", abreJanela)
 
 
 
