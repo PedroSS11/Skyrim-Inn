b4a805430663601629f78d4d5e5efcc9cc3397e2
Pedro Henrique <contatophss11@gmail.com>
2022-09-07 18:11:42 -0300

    controlador abrir page cidades

--- js/wiki.js
+++ js/wiki.js
@@ -1,81 +1,69 @@
-const linkCity = document.getElementsByClassName('linkLocations')
-//const cidades = [{id:1, cidade:'dawnstar', url:'index.html'}]
-
-
-const dawnstar = document.getElementById('dawnstar')
-const a = () => {
-    let cidades = []
-    for(i in linkCity) {
-    
-    
-        if(linkCity[i].id) {
-    
-            //console.log(linkCity[i].id)
-    
-            cidades.push(linkCity[i].id)
-    
-            console.log(cidades)
-    
-    
-        } 
+// Controla a exibição das pages das cidades (escolhe qual html abrir)
+const cidadesPages = () => {
+    //Dawnstar
+    const dawnstarTag = document.getElementById('dawnstar')
+    const abreDawsntar = () => {
+        window.open('dawnstar.html')
     }
+    dawnstarTag.addEventListener("click", abreDawsntar)
+
+    //Falkeath
+    const falkeathTag = document.getElementById('falkeath')
+    const abreFalkeath = () => {
+        window.open('index.html')
+    }
+    falkeathTag.addEventListener("click", abreFalkeath)
+
+    //Markarth
+    const markarthTag = document.getElementById('markarth')
+    const abreMarkarth = () => {
+        window.open('index.html')
+    }
+    markarthTag.addEventListener("click", abreMarkarth)
+
+    //Morthal
+    const morthalTag = document.getElementById('morthal')
+    const abreMorthal = () => {
+        window.open('index.html')
+    }
+    morthalTag.addEventListener("click", abreMorthal)
+
+    //Riften
+    const riftenTag = document.getElementById('riften')
+    const abreRiften = () => {
+        window.open('index.html')
+    }
+    riftenTag.addEventListener("click", abreRiften)
+
+    //Solitude
+    const solitudeTag = document.getElementById('solitude')
+    const abreSolitude = () => {
+        window.open('index.html')
+    }
+    solitudeTag.addEventListener("click", abreSolitude)
+
+    //Whiterun
+    const whiterunTag = document.getElementById('whiterun')
+    const abreWhiterun = () => {
+        window.open('index.html')
+    }
+    whiterunTag.addEventListener("click", abreWhiterun)
+
+    //Windhelm
+    const windhelmTag = document.getElementById('windhelm')
+    const abreWindhelm = () => {
+        window.open('index.html')
+    }
+    windhelmTag.addEventListener("click", abreWindhelm)
+
+    //Winterhold
+    const winterholdTag = document.getElementById('winterhold')
+    const abreWinterhold = () => {
+        window.open('index.html')
+    }
+    winterholdTag.addEventListener("click", abreWinterhold)
 }
-
-a()
-
-
-
-// TIPO ISSOOOOO
-// const dawnstar = document.getElementById('dawnstar')
-// const abreJanela = () => {
-//     let site = cidades[0].url
-//     window.open(site)
-// }
-// dawnstar.addEventListener("click", abreJanela)
+cidadesPages()
 
 
 
-
-
-
-
-
-//console.log(linkCity)
-
-// for(i in linkCity) {
-
-//     let cidades = {}
-//     let num = 1
-
-//     if(linkCity[i].id) {
-
-//         console.log(linkCity[i].id)
-
-//         cidades.push(linkCity[i].id + num)
-
-//         console.log(cidades)
-
-
-//     } 
-
-
-
-
-
-// }
-
-
-
-
-// const abreJanela = () => {
-//     let janela = ''
-//     if (dawnstar.id === 'dawnstar') {
-//        janela = 'index.html'
-//        window.open(janela) 
-//     } else {
-//         alert("nao encontrado")
-//     }
-    
-// }
-
-//dawnstar.addEventListener("click", abreJanela)
